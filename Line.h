7371fea2502f9a70a4f145b034ac0e354a779e47
chang <ayaglch@163.com>
2019-12-10 09:32:13 +0800

    update by liu

---
+++ Line.h
@@ -0,0 +1,12 @@
+#ifndef LINE_H_INCLUDED
+#define LINE_H_INCLUDED
+class Line
+{
+    int RoterId1;
+    int RoterId2;
+    int weight;
+    Line(int id1,int id2);
+};
+
+
+#endif // LINE_H_INCLUDED
