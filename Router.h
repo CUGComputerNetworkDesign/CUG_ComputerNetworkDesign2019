5cfa0f4dfe84ad2477dd85a52cd99d3aea46758b
Eline <472379433@qq.com>
2019-12-17 10:18:33 +0800

    Router的函数实现 段

--- Router.h
+++ Router.h
@@ -4,16 +4,17 @@
 class Router
 {
 private:
-    int RouterIP[3];
-    bool Status;
-    Line *LineHead;
-    RTable *RTableHead;
+    int RouterIP[4]; //IP地址
+    bool Status;     //状态
+    Line *LineHead;   //邻接表
+    RouterTable *RTableHead;//路由表
 public:
     Router(int *IP);               //只初始化变量
     void BuildLine(int *IP,int weight);      //为路由器添加一个新的物理连接
+    bool DeleteLine(int *IP);                //删除一条边
     void RoterOffLine(void){Status=0;}      //路由器下线,状态置0
     void RoterOnline(void){Status=1;}       //路由器上线,状态置1
-    void UpdateRtable(int IP,Rtable* head); //更新路由表
+    void UpdateRtable(int * IP,Routertable* head); //更新路由表
 };
 
 
