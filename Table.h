aa68092cd5a1935e01c9868e8df72d2069f40b67
liyueyao1015 <43902271+liyueyao1015@users.noreply.github.com>
2019-12-17 10:51:23 +0800

    关于IP的修改

--- Table.h
+++ Table.h
@@ -9,7 +9,7 @@ class Table
    Router*data;                 //指向一个路由器实例。
    Table*next;                  //链表的next指针
    Table()
-   {  Table*next=NULL;  } //构造函数，调用路由器的构造函数。
+   {Table*next=NULL;} //构造函数，调用路由器的构造函数。
 };
 
 #endif
