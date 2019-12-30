405f87e059b72d53c94b16f8339b7ef486635138
Eline <472379433@qq.com>
2019-12-30 10:00:16 +0800

    Duan

--- Router_Router.cpp
+++ Router_Router.cpp
@@ -1,4 +1,6 @@
 #include"Router.h"
+#include<iostream>
+using namespace std;
 Router::Router(int *IP) //¹¹Ôìº¯Êý
 {
     for(int i=0;i<4;i++)
@@ -7,5 +9,6 @@ Router::Router(int *IP) //¹¹Ôìº¯Êý
     RTableHead = nullptr;
     RTableHead=new RouterTable(IP,IP,0,RTableHead);
     LineHead = nullptr;
+    cout<<"Duan!!!"<<endl;
     return ;
 }
