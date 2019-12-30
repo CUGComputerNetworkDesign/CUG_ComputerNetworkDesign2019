b7ca0bd30b67686ecedce66e7161728790736da1
Eline <472379433@qq.com>
2019-12-30 09:29:53 +0800

    delete

--- Topo_Topo.cpp
+++
@@ -1,9 +0,0 @@
-#include"Topo.h"
-Topo::Topo(){
-    head = new Table;
-  //  cout<<"构造函数"<<endl;
-    string filename1="node.txt";
-    routerFile(filename1);
-    string filename2="network.txt";
-    networkFile(filename2);
-}
